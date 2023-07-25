8435cfbc89f693c20b3c34c4e81b0614862f424a
BaghdadDev <baghdadhamdoud@gmail.com>
2023-07-25 20:49:47 +0100

    First Message

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div
+      className={
+        "flex w-full flex-col items-center bg-slate-800 text-yellow-400"
+      }
+    >
+      <p className={"font-fascinate-inline text-[200px]"}>ARES</p>
+      <p className={"text-9xl"}>ARES</p>
+      <p>GYM</p>
+    </div>
+  );
+}
+
+export default Banner;
