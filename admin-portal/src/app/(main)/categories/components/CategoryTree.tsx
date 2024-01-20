0641c538b6e13d98eab3bb0405eb11a5fa089d1e
Baghdad <HamzaLinge@gmail.com>
2024-01-20 15:48:23 +0100

    Delete @dnd-kit/core

--- admin-portal/src/app/(main)/categories/components/CategoryTree.tsx
+++ admin-portal/src/app/(main)/categories/components/CategoryTree.tsx
@@ -3,18 +3,9 @@
 import CategoryNode from "@/app/(main)/categories/components/CategoryNode";
 import { cn } from "@/lib/utils";
 
-import { useDroppable } from "@dnd-kit/core";
-
 export default function CategoryTree({ categories }) {
-  const { isOver, setNodeRef } = useDroppable({
-    id: "droppable",
-  });
-  const styleDroppable = {
-    color: isOver ? "green" : undefined,
-  };
-
   return (
-    <div ref={setNodeRef} className={cn("w-full")} style={styleDroppable}>
+    <div className={cn("w-full")}>
       {categories.map((category) => (
         <CategoryNode key={category.name} category={category} />
       ))}
--- admin-portal/src/app/(main)/categories/components/DnDCategoryTree.tsx
+++ admin-portal/src/app/(main)/categories/components/DnDCategoryTree.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import React from "react";
-import { DndContext } from "@dnd-kit/core";
-
 import CategoryTree from "@/app/(main)/categories/components/CategoryTree";
 
 export default function DnDCategoryTree({ categories }) {
-  return (
-    <DndContext>
-      <CategoryTree categories={categories} />
-    </DndContext>
-  );
+  return <CategoryTree categories={categories} />;
 }
